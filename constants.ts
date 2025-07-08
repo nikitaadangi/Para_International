7e444011f8ca4beb59ce98f3ed8293284289a85c
nikitaadangi <nikita.dangi0604@gmail.com>
2025-07-08 15:14:14 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -7,44 +7,44 @@ export const PRODUCTS_DATA: Product[] = [
     id: 1,
     name: 'Elegant Marble Collection',
     category: 'Marble',
-    src='./images/G01.png',
+    src="/Para_International/images/G01.png",
     description: 'A refined blend of contemporary toned and classic charm for modern spaces.'
   },
   {
     id: 2,
     name: 'Earthy & Warm Marble Collection',
     category: 'Marble',
-    src='./images/G02.png',
+    src="/Para_International/images/G02.png",
     description: 'Infused with natural texture and warm hues, perfect for timeless interiors.'
   },
   {
     id: 3,
     name: 'Premium Designer Marble',
     category: 'Marble',
-    src='./images/G03.png',
+    src="/Para_International/images/G03.png",
     description: 'Bold aesthetics and statement patterns to elevate any architectural vision.'
   }
 ];
 
 export const GALLERY_IMAGES: GalleryImage[] = [
-  { id: 1, src:'./images/G01.png', alt: '' },
-  { id: 2, src:'./images/G02.png', alt: '' },
-  { id: 3, src:'./images/G03.png', alt: '' },
-  { id: 4, src:'./images/G04.png', alt: '' },
-  { id: 5, src:'./images/G05.png', alt: '' },
-  { id: 6, src:'./images/G06.png', alt: '' },
-  { id: 7, src:'./images/G07.png', alt: '' },
-  { id: 8, src:'./images/G08.png', alt: '' },
-  { id: 9, src:'./images/G09.png', alt: '' },
-  { id: 10, src:'./images/G10.png', alt: '' },
-  { id: 11, src:'./images/G11.png', alt: '' },
-  { id: 12, src:'./images/G12.png', alt: '' },
-  { id: 13, src:'./images/G13.png', alt: '' },
-  { id: 14, src:'./images/G14.png', alt: '' },
-  { id: 15, src:'./images/G15.png', alt: '' },
-  { id: 16, src:'./images/G16.png', alt: '' },
-  { id: 17, src:'./images/G17.png', alt: '' },
-  { id: 18, src:'./images/G18.png', alt: '' }
+  { id: 1, src:"/Para_International/images/G01.png", alt: '' },
+  { id: 2, src:'/Para_International/images/G02.png', alt: '' },
+  { id: 3, src:'/Para_International/images/G03.png', alt: '' },
+  { id: 4, src:'/Para_International/images/G04.png', alt: '' },
+  { id: 5, src:'/Para_International/images/G05.png', alt: '' },
+  { id: 6, src:'/Para_International/images/G06.png', alt: '' },
+  { id: 7, src:'/Para_International/images/G07.png', alt: '' },
+  { id: 8, src:'/Para_International/images/G08.png', alt: '' },
+  { id: 9, src:'/Para_International/images/G09.png', alt: '' },
+  { id: 10, src:'/Para_International/images/G10.png', alt: '' },
+  { id: 11, src:'/Para_International/images/G11.png', alt: '' },
+  { id: 12, src:'/Para_International/images/G12.png', alt: '' },
+  { id: 13, src:'/Para_International/images/G13.png', alt: '' },
+  { id: 14, src:'/Para_International/images/G14.png', alt: '' },
+  { id: 15, src:'/Para_International/images/G15.png', alt: '' },
+  { id: 16, src:'/Para_International/images/G16.png', alt: '' },
+  { id: 17, src:'/Para_International/images/G17.png', alt: '' },
+  { id: 18, src:'/Para_International/images/G18.png', alt: '' }
 ];
 
 export const TESTIMONIALS_DATA: Testimonial[] = [
